b4e4b384cdc98d207aaabc01508ac013875112d7
jw <jwlee1728@gmail.com>
2017-03-04 22:17:35 +0900

    db config

--- src/main/java/com/ljw/common/config/AppConfig.java
+++ src/main/java/com/ljw/common/config/AppConfig.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
@@ -18,6 +17,8 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -39,18 +40,24 @@ public class AppConfig extends WebMvcConfigurerAdapter implements TransactionMan
 	@Bean
 	public DataSource dataSource() {
 		
-		BasicDataSource ds = new BasicDataSource();
+//		BasicDataSource ds = new BasicDataSource();
 	
-		ds.setDriverClassName("org.mariadb.jdbc.Driver");
-		ds.setUrl("jdbc:mysql://iothome.iptime.org:3306/iot");
-		ds.setUsername("iot");
-		ds.setPassword("lee3319");
+//		ds.setDriverClassName("org.mariadb.jdbc.Driver");
+//		ds.setUrl("jdbc:mysql://iothome.iptime.org:3306/iot");
+//		ds.setUsername("iot");
+//		ds.setPassword("lee3319");
 		
 //		ds.setDriverClassName("${db.driver}");
 //		ds.setUrl("${db.url}");
 //		ds.setUsername("${db.user}");
 //		ds.setPassword("${db.password}");
-		return ds;
+		
+		return new EmbeddedDatabaseBuilder()
+				.setName("sensordb") // DB 이름 설정
+				.setType(EmbeddedDatabaseType.HSQL) // DB 종류 설정
+				.addScript("com/ljw/iot/data/schema.sql") // 스키마 스크립트 추가
+				.addScript("com/ljw/iot/data/data.sql") // 데이터 스크립트 추가
+				.build();
 	}
 
 	@Bean
