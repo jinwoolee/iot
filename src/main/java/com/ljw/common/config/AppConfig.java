5fb75de0375e4d93b7b217d1ef1cbf3a4c67e66e
jw <jwlee1728@gmail.com>
2017-03-05 17:46:54 +0900

    db property

--- src/main/java/com/ljw/common/config/AppConfig.java
+++ src/main/java/com/ljw/common/config/AppConfig.java
@@ -5,20 +5,22 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -35,29 +37,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 												  @Filter(Controller.class),
 												  @Filter(ControllerAdvice.class)})
 @Configuration
-//@PropertySource(value={"classpath:com/ljw/common/config/db.properties"})
+@PropertySource(value={"classpath:com/ljw/common/config/db.properties"})
 public class AppConfig extends WebMvcConfigurerAdapter implements TransactionManagementConfigurer{
-	@Bean
-	public DataSource dataSource() {
-		
-//		BasicDataSource ds = new BasicDataSource();
+
+	@Autowired
+    private Environment env;
 	
-//		ds.setDriverClassName("org.mariadb.jdbc.Driver");
-//		ds.setUrl("jdbc:mysql://iothome.iptime.org:3306/iot");
-//		ds.setUsername("iot");
-//		ds.setPassword("lee3319");
+	@Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+	 
+	@Bean
+	public DataSource dataSource() {	
+		BasicDataSource ds = new BasicDataSource();
 		
-//		ds.setDriverClassName("${db.driver}");
-//		ds.setUrl("${db.url}");
-//		ds.setUsername("${db.user}");
-//		ds.setPassword("${db.password}");
-		
-		return new EmbeddedDatabaseBuilder()
-				.setName("sensordb") // DB 이름 설정
-				.setType(EmbeddedDatabaseType.HSQL) // DB 종류 설정
-				.addScript("com/ljw/iot/data/schema.sql") // 스키마 스크립트 추가
-				.addScript("com/ljw/iot/data/data.sql") // 데이터 스크립트 추가
-				.build();
+		ds.setDriverClassName(env.getProperty("db.driver"));
+		ds.setUrl(env.getProperty("db.url"));
+		ds.setUsername(env.getProperty("db.user"));
+		ds.setPassword(env.getProperty("db.password"));
+		return ds;
 	}
 
 	@Bean
