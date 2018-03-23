22722b8d1a77ba7aec22fb6e8e6104c58bac23a5
jw <jwlee1728@gmail.com>
2018-03-23 18:15:34 +0900

    first commit

---
+++ src/main/java/org/iptime/iothome/IothomeApplication.java
@@ -0,0 +1,12 @@
+package org.iptime.iothome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IothomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IothomeApplication.class, args);
+	}
+}
