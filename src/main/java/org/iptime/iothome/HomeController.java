72c1fc4b7b4a65eb122782a4802fdacb20c3ab5d
jw <jwlee1728@gmail.com>
2018-05-01 07:35:09 +0900

    question 데이터 초기화 index question list 추

--- src/main/java/org/iptime/iothome/HomeController.java
+++ src/main/java/org/iptime/iothome/HomeController.java
@@ -1,13 +1,21 @@
 package org.iptime.iothome;
 
+import javax.annotation.Resource;
+
+import org.iptime.iothome.domain.QuestionRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+    @Resource
+    QuestionRepository questionRepository; 
+    
     @RequestMapping("")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("questions", questionRepository.findAll());
+        
         return "index";
     }
 }
