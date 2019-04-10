a05bf22422d3ea9a156357a1ca57f32d8fc55fd3
jwlee <jwlee1728@gmail.com>
2019-04-10 13:17:25 +0900

    mariadb -> firebase 진행중

--- src/test/java/com/ljw/iot/httpcleint/HttpClientTest.java
+++ src/test/java/com/ljw/iot/httpcleint/HttpClientTest.java
@@ -2,9 +2,11 @@ package com.ljw.iot.httpcleint;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -21,48 +23,72 @@ import org.slf4j.LoggerFactory;
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 
+
+
 public class HttpClientTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(HttpClientTest.class);
+	private static final Logger logger = LoggerFactory.getLogger(HttpClientTest.class);
 
-    // get test
-    @Test
-    public void httpClientRequestTest() throws ClientProtocolException, IOException {
-	HttpClient httpClient = HttpClients.createDefault();
-	HttpGet httpGet = new HttpGet("https://github.com/jinwoolee");
+	// get test
+	//@Test
+	public void httpClientRequestTest() throws ClientProtocolException, IOException {
+		HttpClient httpClient = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet("https://github.com/jinwoolee");
 
-	ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+		ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
 
-	    @Override
-	    public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
-		HttpEntity entity = response.getEntity();
-		return entity != null ? EntityUtils.toString(entity) : null;
-	    }
-	};
+			@Override
+			public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+				HttpEntity entity = response.getEntity();
+				return entity != null ? EntityUtils.toString(entity) : null;
+			}
+		};
 
-	String responseString = httpClient.execute(httpGet, responseHandler);
-	logger.debug("responseString : {}", responseString);
-	assertNotNull(responseString);
-    }
+		String responseString = httpClient.execute(httpGet, responseHandler);
+		logger.debug("responseString : {}", responseString);
+		assertNotNull(responseString);
+	}
 
-    //form submit test
-    @Test
-    public void formSubmitTest() throws IOException {
-	
-	InputStream serviceAccount = HttpClientTest.class.getClassLoader().getResourceAsStream("com/ljw/iot/config/firebase/iot-dust-sensor-account.json");
-	GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
-	FirebaseOptions options = new FirebaseOptions.Builder()
-	    .setCredentials(credentials)
-	    .build();
-	FirebaseApp.initializeApp(options);
+	// read firebase test
+	@Test
+	public void readFirebaseTest() throws IOException, InterruptedException, ExecutionException {
 
-	Firestore db = FirestoreClient.getFirestore();
-	
-	ApiFuture<QuerySnapshot> query = db.collection("dustMeasure").get();
-    }
+		InputStream serviceAccount = HttpClientTest.class.getClassLoader().getResourceAsStream("com/ljw/iot/config/firebase/iot-dust-sensor-account.json");
+		GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+		
+		//getDate메소드를 호출하면 convertToDateIfNecessary(), areTimestampsInSnapshotsEnabled() 설정에 의해,
+		//date로 자동 형변환됨. 
+		//timestamp 타입으로 받고 싶으면 setTimestampsInSnapshotsEnabled(true) 설정을 적용 해야함
+		FirebaseOptions options = new FirebaseOptions.Builder()
+										.setCredentials(credentials)
+										.setFirestoreOptions(FirestoreOptions.newBuilder()
+																.setTimestampsInSnapshotsEnabled(true).build())
+										.build();
+		
+		FirebaseApp.initializeApp(options);
+
+		Firestore db = FirestoreClient.getFirestore();
+
+
+		ApiFuture<QuerySnapshot> query = db.collection("dustMeasure").get();
+		QuerySnapshot querySnapshot = query.get();
+		
+		List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+		for (QueryDocumentSnapshot document : documents) {
+			
+			logger.debug("document.getId() : {} ", document.getId());
+			logger.debug("aqi : {} ", document.getLong("aqi"));
+			logger.debug("measure : {} ", document.getLong("measure"));
+			logger.debug("measure_id : {} ", document.getLong("measure_id"));
+			logger.debug("reg_dt : {} ", document.getTimestamp("reg_dt"));
+			//logger.debug("reg_dt : {} ", document.getDate("reg_dt"));
+		}
+	}
 }
