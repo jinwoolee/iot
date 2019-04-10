beb15c358ba7e875a49666069f7e611cb3b45d47
jw <jwlee1728@gmail.com>
2019-04-10 09:04:43 +0900

    maraidb -> firebase 진행중

--- src/test/java/com/ljw/iot/httpcleint/HttpClientTest.java
+++ src/test/java/com/ljw/iot/httpcleint/HttpClientTest.java
@@ -2,7 +2,9 @@ package com.ljw.iot.httpcleint;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -16,28 +18,51 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
 public class HttpClientTest {
 
-	private static final Logger logger = LoggerFactory.getLogger(HttpClientTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(HttpClientTest.class);
 
-	@Test
-	public void httpClientRequestTest() throws ClientProtocolException, IOException {
-		HttpClient httpClient = HttpClients.createDefault();
-		HttpGet httpGet = new HttpGet("https://github.com/jinwoolee");
+    // get test
+    @Test
+    public void httpClientRequestTest() throws ClientProtocolException, IOException {
+	HttpClient httpClient = HttpClients.createDefault();
+	HttpGet httpGet = new HttpGet("https://github.com/jinwoolee");
 
-		ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+	ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
 
-			@Override
-			public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
-				HttpEntity entity = response.getEntity();
-				return entity != null ? EntityUtils.toString(entity) : null;
-			}
-		};
+	    @Override
+	    public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+		HttpEntity entity = response.getEntity();
+		return entity != null ? EntityUtils.toString(entity) : null;
+	    }
+	};
 
-		String responseString = httpClient.execute(httpGet, responseHandler);
-		logger.debug("responseString : {}", responseString);
-		assertNotNull(responseString);
-	}
-		
+	String responseString = httpClient.execute(httpGet, responseHandler);
+	logger.debug("responseString : {}", responseString);
+	assertNotNull(responseString);
+    }
 
+    //form submit test
+    @Test
+    public void formSubmitTest() throws IOException {
+	
+	InputStream serviceAccount = HttpClientTest.class.getClassLoader().getResourceAsStream("com/ljw/iot/config/firebase/iot-dust-sensor-account.json");
+	GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+	FirebaseOptions options = new FirebaseOptions.Builder()
+	    .setCredentials(credentials)
+	    .build();
+	FirebaseApp.initializeApp(options);
+
+	Firestore db = FirestoreClient.getFirestore();
+	
+	ApiFuture<QuerySnapshot> query = db.collection("dustMeasure").get();
+    }
 }
