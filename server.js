5c75f5f216b97e3899670e2df862db7aa7bdea97
jw <jwlee1728@gmail.com>
2019-06-19 07:55:13 +0900

    sensor measure json post body parse

--- server.js
+++ server.js
@@ -19,32 +19,6 @@ admin.initializeApp({
 var db = admin.firestore();
 
 
-Date.prototype.format = function(f) {
-    if (!this.valueOf()) return " ";
- 
-    var weekName = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
-    var d = this;
-     
-    return f.replace(/(yyyy|yy|MM|dd|E|hh|mm|ss|a\/p)/gi, function($1) {
-        switch ($1) {
-            case "yyyy": return d.getFullYear();
-            case "yy": return (d.getFullYear() % 1000).zf(2);
-            case "MM": return (d.getMonth() + 1).zf(2);
-            case "dd": return d.getDate().zf(2);
-            case "E": return weekName[d.getDay()];
-            case "HH": return d.getHours().zf(2);
-            case "hh": return ((h = d.getHours() % 12) ? h : 12).zf(2);
-            case "mm": return d.getMinutes().zf(2);
-            case "ss": return d.getSeconds().zf(2);
-            case "a/p": return d.getHours() < 12 ? "오전" : "오후";
-            default: return $1;
-        }
-    });
-};
- 
-String.prototype.string = function(len){var s = '', i = 0; while (i++ < len) { s += this; } return s;};
-String.prototype.zf = function(len){return "0".string(len - this.length) + this;};
-Number.prototype.zf = function(len){return this.toString().zf(len);};
 
 app.get("/", function(req, res){
     res.render("./sensorMeasure", {});
@@ -70,6 +44,25 @@ app.get("/sensor/getMeasure", function(req, res){
         {dt:11, measure:200},
         {dt:12, measure:300} ] };
         res.status(200).json(monthlyData);
+});
+
+//json 입력 테스트
+app.post('/sensor/measure', function(req, res){
+
+    console.log("req.body : ", req.body );
+    var measure = req.body;
+    
+    var dt = new Date();
+
+    var yyyyMMddDocRef = db.collection('dustMeasure').doc(dt.format("yyyyMMdd"));
+    var hDocRef = yyyyMMddDocRef.collection(dt.format("HH")).doc(dt.format("mm"));
+    
+    measure.reg_dt =  new Date();
+    
+    console.log("measure ", measure );
+    var setDoc = hDocRef.set(measure);
+
+    res.send('Hello World post');
 });
 
 app.get('/sensor/measure', function(req, res){
@@ -98,3 +91,31 @@ app.get('/sensor/measure', function(req, res){
   
   app.listen(3000);
 
+
+
+  Date.prototype.format = function(f) {
+    if (!this.valueOf()) return " ";
+ 
+    var weekName = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+    var d = this;
+     
+    return f.replace(/(yyyy|yy|MM|dd|E|hh|mm|ss|a\/p)/gi, function($1) {
+        switch ($1) {
+            case "yyyy": return d.getFullYear();
+            case "yy": return (d.getFullYear() % 1000).zf(2);
+            case "MM": return (d.getMonth() + 1).zf(2);
+            case "dd": return d.getDate().zf(2);
+            case "E": return weekName[d.getDay()];
+            case "HH": return d.getHours().zf(2);
+            case "hh": return ((h = d.getHours() % 12) ? h : 12).zf(2);
+            case "mm": return d.getMinutes().zf(2);
+            case "ss": return d.getSeconds().zf(2);
+            case "a/p": return d.getHours() < 12 ? "오전" : "오후";
+            default: return $1;
+        }
+    });
+};
+ 
+String.prototype.string = function(len){var s = '', i = 0; while (i++ < len) { s += this; } return s;};
+String.prototype.zf = function(len){return "0".string(len - this.length) + this;};
+Number.prototype.zf = function(len){return this.toString().zf(len);};
